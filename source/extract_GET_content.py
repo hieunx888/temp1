600e79b86e1dc50988d467d0693fee20b99f7b50
hieunx888 <162667328+hieunx888@users.noreply.github.com>
2024-03-22 10:56:06 +0700

    Delete source directory

--- source/extract_GET_content.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse
-
-def extract_params(request):
-    url = (request.split('\n')[0]).split(" ")[1]
-    parsed_url = urllib.parse.urlparse(url)
-    query_params = urllib.parse.parse_qs(parsed_url.query)
-    query_params = list(query_params.values())
-    valueList = [item for sublist in query_params for item in sublist]
-    return valueList
